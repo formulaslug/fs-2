ccb76d19d71525aa5ef9c67f3f30482026889ff4
Cole Lewis <colestephenlewis@gmail.com>
2024-04-14 17:50:08 -0700

    working bms for l432kc

--- bms/src/Main.cpp
+++ bms/src/Main.cpp
@@ -1,51 +1,27 @@
-#include "BmsConfig.h"
+#include <array>
+#include <cstdio>
+#include <vector>
+#include <iostream>
 
 #include "mbed.h"
-#include "rtos.h"
 
-//#include "spdlog/spdlog.h"
-
+#include "LTC681xParallelBus.h"
 #include "BmsThread.h"
-#include "Can.h"
-//#include "CANOptions.h"
-#include "LTC681xBus.h"
-#include "Event.h"
-#include <cstdint>
-
-// When car is off maybe one reading every 10 sec
-// when car is on 10 readings per second
-
-// TODO: map 12 cells to number of cells
-// TODO: change m_chips to array rather than vector
-// TODO: publish fault states
-// TODO: SoC tracking using Ah counting and voltage measuring
-
-// Fault states:
-// - AMS
-// - IMD
-//
-// * Over temp
-// - Under temp
-// * Over voltage
-// * Under voltage
-//
-// * Failed init
-// * Comm fail
 
 
 CAN* canBus;
-DigitalOut* bmsFault;
-DigitalOut* chargerControl;
 
 void initIO();
 
-int main() {
-  // Init all io pins
-  initIO();
-  
-  canBus->write(BMSCellStartup());
+CircularBuffer<CANMessage, 32> canqueue;
 
-  ThisThread::sleep_for(1s);
+uint8_t canCount;
+
+int main() {
+  printf("main\n");
+  initIO();
+  printf("initIO\n");
+
 
   SPI* spiDriver = new SPI(BMS_PIN_SPI_MOSI,
                            BMS_PIN_SPI_MISO,
@@ -56,89 +32,30 @@ int main() {
   auto ltcBus = LTC681xParallelBus(spiDriver);
 
   auto canMailbox = new BmsEventMailbox();
-  auto uiMailbox = new BmsEventMailbox();
-  std::vector<BmsEventMailbox*> mailboxes = { canMailbox, uiMailbox };
+  std::vector<BmsEventMailbox *> mailboxes = {canMailbox};
 
   Thread bmsThreadThread;
   BMSThread bmsThread(ltcBus, 1, mailboxes);
   bmsThreadThread.start(callback(&BMSThread::startThread, &bmsThread));
 
-  DigitalOut led(LED1);
-  // Flash LEDs to indicate startup
-  for (int i = 0; i < 4; i++) {
-    led = 1;
-    ThisThread::sleep_for(50ms);
-    led = 0;
-    ThisThread::sleep_for(50ms);
-  }
+  std::array<int8_t, BMS_BANK_COUNT * BMS_BANK_TEMP_COUNT> allTemps;
+  osThreadSetPriority(osThreadGetId(), osPriorityHigh7);
+  Timer t;
+  t.start();
+  while (1) {
 
-  while(true) {
-    auto event = canMailbox->get();
-    if (event.status == osEventMessage) {
-      BmsEvent* msg = (BmsEvent*)event.value.p;
-
-      switch(msg->getType()) {
-      case BmsEventType::VoltageMeasurement: {
-        auto converted = static_cast<VoltageMeasurement*>(msg);
-        
-        printf("Voltages: \n");
-        for (int i = 0; i < BMS_BANK_COUNT; i++) {
-          for(int j = 0; j < BMS_BANK_CELL_COUNT; j++) {
-            printf("%4dmV ", converted->voltageValues[(i * BMS_BANK_CELL_COUNT) + j]);
-          }
-          printf("\n");
+    while (!canqueue.empty()) {
+        CANMessage msg;
+        canqueue.pop(msg);
+        switch(msg.id) {
+          default:
+            break;
         }
-        converted->~VoltageMeasurement();
-        break;
-      }
-      case BmsEventType::TemperatureMeasurement: {
-        auto converted = static_cast<TemperatureMeasurement*>(msg);
-        
-        printf("Temperatures: \n");
-        for (int i = 0; i < BMS_BANK_COUNT; i++) {
-          for(int j = 0; j < BMS_BANK_TEMP_COUNT; j++) {
-            int8_t val = converted->temperatureValues[(i * BMS_BANK_CELL_COUNT) + j];
-            if(val) {
-              printf("%2d ", val);
-            } else {
-              printf("XX ");
-            }
-          }
-          printf("\n");
-        }
-        converted->~TemperatureMeasurement();
-        break;
-      }
-      default:
-        break;
-      }
-      delete msg;
     }
+    ThisThread::sleep_for(50 - (t.read_ms()%50));
   }
-  bmsThreadThread.join();
 }
 
 void initIO() {
-  printf("INIT\n");
-  
-  canBus = new CAN(BMS_PIN_CAN_RX, BMS_PIN_CAN_TX, BMS_CAN_FREQUENCY);
-
-  bmsFault = new DigitalOut(BMS_PIN_BMS_FLT);
-  
-  chargerControl = new DigitalOut(BMS_PIN_CHARGER_CONTROL);
-  
-  // Set modes for IO
-  /*
-  palSetLineMode(LINE_BMS_FLT_LAT, PAL_MODE_INPUT);
-  palSetLineMode(LINE_IMD_STATUS, PAL_MODE_INPUT);
-  palSetLineMode(LINE_IMD_FLT_LAT, PAL_MODE_INPUT);
-  palSetLineMode(LINE_SIG_CURRENT, PAL_MODE_INPUT_ANALOG);
-  */
-
-  // Reset BMS fault line
-  bmsFault->write(1);
-  bmsFault->write(0);
-
-  // Enable charging
-  chargerControl->write(0);
+    canBus = new CAN(BMS_PIN_CAN_RX, BMS_PIN_CAN_TX, BMS_CAN_FREQUENCY);
 }

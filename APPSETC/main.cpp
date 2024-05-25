06d3b2a0d8cfb8036e57fed9aa85eb37cd803c2c
DustinWang1 <wangd385@gmail.com>
2024-05-25 00:02:15 -0700

    Untested implausability tester. Reformatted to disclude the use of multithreading.

--- APPSETC/main.cpp
+++ APPSETC/main.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "mbed.h"
+#include <chrono>
 #include <cstdint>
 
 #if !DEVICE_CAN
@@ -17,6 +18,12 @@
 #define CAN_TX_PIN D2
 #define CAN_FREQ 500000
 
+// Constant tested range of values for pedal travel calculation
+const float HE1_LOW = .28;
+const float HE1_HIGH = .85;
+const float HE2_LOW = .21;
+const float HE2_HIGH = .65;
+
 AnalogIn HE1(A1);
 AnalogIn HE2(A0);
 AnalogIn brakes(A2);
@@ -28,7 +35,6 @@ CAN* canBus;
 
 bool TS_Ready = false; 
 bool Motor_On = false;
-bool Implausability = false;
 
 void initIO();
 void canRX();
@@ -50,7 +56,7 @@ void runRTDS() {
     printf("RUNNING RTDS");
     test_led.write(true);
     RTDScontrol.write(true);
-    wait_us(3000000);
+    wait_us(1000000);
     test_led.write(false);
     RTDScontrol.write(false);
     printf("FINISHED RTDS");
@@ -58,44 +64,30 @@ void runRTDS() {
 
 void cockpit_switch_high() {
     wait_us(10000);
-    int signal = Cockpit_D.read();
-    if(Cockpit_D.read() == 0) {
-        return;
-    }
+    if(Cockpit_D.read() == 0) {return;}
     test_led.write(true);
     if (TS_Ready && brakes.read() >= BRAKE_TOL) {
         Motor_On = true;
+        runRTDS();
+    } else {
+        wait_us(10000);
     }
-    wait_us(10000);
     test_led.write(false);
-}
-
-void check_start_conditions() {
-    CANMessage msg;
-    int counter = 0;
-    while (Motor_On == false) {
-        if(counter >= 10000) { 
-            printf("Still Waiting \n"); 
-            counter = 0;
-        }
-
-        if(brakes.read() > BRAKE_TOL) {
-            printf("Brakes pressed.\n");
-        }
-
-    }
 }
 
 void cockpit_switch_low() {
     wait_us(10000);
     int signal = Cockpit_D.read();
-    if(Cockpit_D.read() == 1) {
-        return;
-    }
+    if(Cockpit_D.read() == 1) {return;}
     test_led.write(true);
     Motor_On = false;
     wait_us(10000);
     test_led.write(false);
+}
+
+void implausability() {
+    printf("implausability occured");
+    Motor_On = false;
 }
 
 float clamp(float value, float low, float high) {
@@ -107,7 +99,7 @@ float clamp(float value, float low, float high) {
 void initIO() {
     canBus = new CAN(CAN_RX_PIN, CAN_TX_PIN, CAN_FREQ);
 
-    queue.call_every( 20ms, &sendThrottle);
+    queue.call_every(20ms, &sendThrottle);
     queue.call_every(100ms, &sendSync);
 }
 
@@ -145,6 +137,53 @@ void sendThrottle() {
     canBus->write(throttleMessage);
 }
 
+void printStatusMessage() {
+    int ts_rdy = TS_Ready ? 1 : 0;
+    int cockpit = Cockpit_D.read();
+    float b = brakes.read();
+
+    printf("Cockpit Switch: %i | TS_RDY: %i | BPSD: %f\n", cockpit, ts_rdy, b);
+}
+
+float getPedalTravel(Timer* implausability_track) {
+    // We calculate in a long-winded fashion for debug purposes
+    float HE1_read = HE1.read();
+    float HE2_read = HE2.read();
+    //printf("HE1: %f  | HE2: %f\n", HE1_read, HE2_read);
+
+    float clamped_HE1 = clamp(HE1_read, HE1_LOW, HE1_HIGH);
+    float clamped_HE2 = clamp(HE2_read, HE2_LOW, HE2_HIGH);
+    float HE1_travel = (clamped_HE1-HE1_LOW) / (HE1_HIGH - HE1_LOW);
+    float HE2_travel = (clamped_HE2-HE2_LOW) / (HE2_HIGH - HE2_LOW);
+    printf("HE1_travel: %f  | HE2_travel: %f\n", HE1_travel, HE2_travel);
+
+    //implausibility if greater than 10% pedal travel diff for more than 100 ms.
+    float pedal_travel = 0.5*(HE1_travel + HE2_travel); // take the avg of the two pedal travels
+    float travel_diff = std::abs(HE1_travel - HE2_travel);
+    
+    if(travel_diff > .1f) {
+        implausability_track->stop();
+        unsigned long long current_time = duration_cast<std::chrono::milliseconds>(implausability_track->elapsed_time()).count();
+        printf("implausability timer: %llu", current_time);
+        if(current_time > 100) {
+            printf("implausability");
+            queue.call(implausability);
+            implausability_track->stop();
+            implausability_track->reset();
+        } else {
+            if(implausability_track->elapsed_time() == std::chrono::microseconds(0)) {
+                implausability_track->start();
+            }
+        }
+        
+    } else {
+        implausability_track->stop();
+        implausability_track->reset();
+    }
+
+    return pedal_travel;
+
+}
 
 int main()
 {   
@@ -153,12 +192,6 @@ int main()
     // Initiate CAN Bus 
     initIO();
     canBus->attach(canRX);
-
-    // Constant tested range of values for pedal travel calculation
-    const float HE1_LOW = .28;
-    const float HE1_HIGH = .85;
-    const float HE2_LOW = .21;
-    const float HE2_HIGH = .65;
 
     // Begin waiting for start conditions
     printf("Waiting for start conditions!\n");
@@ -169,23 +202,12 @@ int main()
     // Event for closing motor
     Cockpit.fall(&cockpit_switch_low);
 
-    //Infinite debug loop that breaks on Motor_On == True
-    check_start_conditions();
-
-    runRTDS();
-    
-    // (NOTE) this is a duplicate it was alr called in initIO? 
-    int syncSend_id = queue.call_every(100ms, &sendSync);
-    int throttleSend_id = queue.call_every(200ms, &sendThrottle);
-
     // forward decl. for timer tracking 100ms implausabilities for rules
-    Timer implausability_track;
+    Timer implausability_timer;
 
-    // Begins sending CAN messages. If this loop is not running, the motor should not be powered.
-    while(Motor_On) {
+    while(1) {
         
-        // This loop only runs if TS rdy == true. If this is the code to interpret can msgs it needs to be 
-        // on a separate thread that has been running from the beginning.
+
         while (!canqueue.empty()) {
             CANMessage msg;
             canqueue.pop(msg);
@@ -193,62 +215,40 @@ int main()
             uint32_t id = msg.id;
             unsigned char* data = msg.data;
 
+            //TODO read ts rdy signal
             switch (id) {
                 default:
                     break;
             }
         }
 
+        if (Motor_On) {
+            float pedalTravel = getPedalTravel(&implausability_timer);
 
-        // We calculate in a long-winded fashion for debug purposes
+            // THROTTLE (uint16 torquedemand (%of0xffff), uint16 maxspeed (rpm), 
+            // bool powerrdy, bool reverse = False, bool forward = True, 
+            // uint8 nbbalive (increment and reset when max)) 
+            // send 20 ms freq
 
-        float HE1_read = HE1.read();
-        float HE2_read = HE2.read();
-        //printf("HE1: %f  | HE2: %f\n", HE1_read, HE2_read);
+            //Sync message  (ID is 0x80) (Msg is 0x00 100 ms freq)
 
-        float clamped_HE1 = clamp(HE1_read, HE1_LOW, HE1_HIGH);
-        float clamped_HE2 = clamp(HE2_read, HE2_LOW, HE2_HIGH);
-        float HE1_travel = (clamped_HE1-HE1_LOW) / (HE1_HIGH - HE1_LOW);
-        float HE2_travel = (clamped_HE2-HE2_LOW) / (HE2_HIGH - HE2_LOW);
-        printf("HE1_travel: %f  | HE2_travel: %f\n", HE1_travel, HE2_travel);
-
-        //implausibility if greater than 10% pedal travel diff for more than 100 ms.
-        float pedal_travel = 0.5*(HE1_travel + HE2_travel); // take the avg of the two pedal travels
-        float travel_diff = std::abs(HE1_travel - HE2_travel);
-        if(travel_diff > .1f) {
-            printf("implausability");
-            implausability_track.stop();
-            unsigned long long current_time = duration_cast<std::chrono::milliseconds>(implausability_track.elapsed_time()).count();
-            printf("implausability timer: %llu", current_time);
-            if(current_time > 100) {
-                Implausability = true;
-                Motor_On = false;
-                implausability_track.reset();
-            } else {
-                implausability_track.start();
-            }
-            
+            powerRdy = Motor_On;
+            motorReverse = false;
+            motorForward = true;
+            torqueDemand = 100 * pedalTravel; // Dunno if it should be between 0 and 1 or 0 and 100
+            maxSpeed = MAXSPEED;
         } else {
-            implausability_track.reset();
+            powerRdy = false;
+            motorReverse = false;
+            motorForward = true;
+            torqueDemand = 0; // Dunno if it should be between 0 and 1 or 0 and 100
+            maxSpeed = MAXSPEED;
+            printStatusMessage();
         }
-        // THROTTLE (uint16 torquedemand (%of0xffff), uint16 maxspeed (rpm), 
-        // bool powerrdy, bool reverse = False, bool forward = True, 
-        // uint8 nbbalive (increment and reset when max)) 
-        // send 20 ms freq
-
-        //Sync message  (ID is 0x80) (Msg is 0x00 100 ms freq)
-
-
-        powerRdy = Motor_On;
-        motorReverse = false;
-        motorForward = true;
-        torqueDemand = 100 * pedal_travel;
-        maxSpeed = MAXSPEED;
 
         queue.dispatch_once();
 
     }
 
-    main();
 }
 
